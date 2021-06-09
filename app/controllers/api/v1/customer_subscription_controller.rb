4df725303089b1079c223151c7b3624a3b58e67f
Adam Bowers <adam.j.bowers@gmail.com>
2021-06-09 15:28:24 -0600

    Remove commented out code

--- app/controllers/api/v1/customer_subscription_controller.rb
+++ app/controllers/api/v1/customer_subscription_controller.rb
@@ -34,28 +34,4 @@ class Api::V1::CustomerSubscriptionController < ApplicationController
   def customer_subscription
     params.require(:customer_subscription).permit(:customer_id, :subscription_id)
   end
-
-  # def check_params
-  #   if !customer_subscription[:customer_id].present? || !customer_subscription[:subscription_id].present?
-  #     render json: {error: "Please provide both a customer id and a subscription id."}, status: 400
-  #   end
-  # end
-
-  # def check_customer_exists
-  #   if !Customer.exists?(customer_subscription[:customer_id])
-  #     render json: {error: "The customer does not exist."}, status: 400
-  #   end
-  # end
-
-  # def check_subscription_exists
-  #   if !Subscription.exists?(customer_subscription[:subscription_id])
-  #     render json: {error: "The subscription does not exist."}, status: 400
-  #   end
-  # end
-
-  # def check_customer_subscription_exists
-  #   if !CustomerSubscription.exists?(customer_id: customer_subscription[:customer_id], subscription_id: customer_subscription[:subscription_id])
-  #     render json: {error: "The customer does not have this subscription."}, status: 400
-  #   end
-  # end
 end
